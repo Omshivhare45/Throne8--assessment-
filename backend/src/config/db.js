964d8114eadadb582087452ef100c974d9c27415
Om Shivhare <omshivhare666@gmail.com>
2026-06-24 09:00:07 +0530

    fixed the db connection error in the config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDB(){
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("DB connected");
+        console.log("Cuccesssfully connected to Database");
     }catch(err){
         console.log("DB connection Error : ", err);
     }
